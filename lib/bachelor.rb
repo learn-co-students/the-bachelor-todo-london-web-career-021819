6b5daae2a1414bffcb5e3d2785518cc64f0c00dc
Ali Madni <ali555m@hotmail.com>
2019-02-13 13:19:14 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,59 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |s, contestants|
+    if s == season
+      contestants.each do |contestant_data|
+        if contestant_data["status"] == "Winner"
+          name_ary = contestant_data["name"].split(" ")
+          return name_ary[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |s, contestants|
+    contestants.each do |contestant_data|
+      if contestant_data["occupation"] == occupation
+        return contestant_data["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |s, contestants|
+    contestants.each do |contestant_data|
+      if contestant_data["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+
+  data.each do |s, contestants|
+    contestants.each do |contestant_data|
+      if contestant_data["hometown"] == hometown
+        return contestant_data["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = 0
+  total = 0
+  data.each do |s, contestants|
+    if s == season
+      contestants.each_with_index do |contestant_data, index|
+        age += contestant_data["age"].to_f
+        total = index + 1
+      end
+    end
+  end
+  return (age / total).round
 end
