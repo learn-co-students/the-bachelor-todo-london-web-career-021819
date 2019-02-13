cb8b6a5e089ff1504a7fbd8c7a7db35d52a86a93
Moses Olawoyin <m.bastford@gmail.com>
2019-02-13 16:09:15 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,25 +1,41 @@
 require "pry"
 def get_first_name_of_season_winner(data, season)
-  data.each do |name|
-    binding.pry
-    if status == "Winner"
-      return[:name]
+  data.each do |period, info|
+    if period == season
+    info.each do|keys, value|
+    keys.each do|info |
+    #binding.pry
+    if keys["status"] == "Winner"
+        return keys["name"].split(" ").first
+         end
+        end
+      end
     end
   end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+   data.each do |season, info|
+     info.each do|info_hash, period|
+      info_hash.each do |name, value|
+       #binding.pry
+        if value["name"] == "name"
+        return value["name"]
+     
+        end
+      end
+    end
+  end
 end
 
-def count_contestants_by_hometown(data, hometown)
-  # code here
-end
+# def count_contestants_by_hometown(data, hometown)
+#   # code here
+# end
 
-def get_occupation(data, hometown)
-  # code here
-end
+# def get_occupation(data, hometown)
+#   # code here
+# end
 
-def get_average_age_for_season(data, season)
-  # code here
-end
+# def get_average_age_for_season(data, season)
+#   # code here
+# end
